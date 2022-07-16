9b58d8b0c410ea32544b35d6c6225dcacb1786fd
thecolorpuce <rileyasp@gmail.com>
2022-07-16 10:47:49 -0700

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	renamed:    5/5-3.py -> 5/5-3.1.py 	new file:   5/5-3.2.py

--- 5/5-3.1.py
+++ 5/5-3.1.py
@@ -1,3 +1,9 @@
 #5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a
 #variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
 
+alienColor = 'red'
+
+if alienColor == 'red':
+    print(True)
+elif alienColor != 'red':
+    print(False)
---
+++ 5/5-3.2.py
@@ -0,0 +1,12 @@
+#5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a
+#variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+
+#Failure version isn't supposed to have an output
+#Just ignoring that 
+alienColor = 'red'
+
+if alienColor == 'red':
+    print(False)
+elif alienColor != 'red':
+    print(True)
