bb157b71354676f3c339cf3f913904f52cd92917
thecolorpuce <rileyasp@gmail.com>
2022-07-16 10:54:24 -0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   5/5-4.1.py 	new file:   5/5-4.2.py

---
+++ 5/5-4.2.py
@@ -0,0 +1,14 @@
+#5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and
+#write an if-else chain.
+
+alienColor = 'green'
+points = []
+
+if alienColor == 'green':
+    points.append(5)
+    print("5 points!")
+elif alienColor != 'green':
+    points.append(10)
+    print("10 points!")
+    
+print(points)
