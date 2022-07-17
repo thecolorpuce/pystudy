ea16171a1848f54626e4252c7b12180a67bb1967
thecolorpuce <rileyasp@gmail.com>
2022-07-17 11:48:57 -0700

    modified:   6/6scrap.py

--- 6/6scrap.py
+++ 6/6scrap.py
@@ -1,9 +1,25 @@
-favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'ruby',
-    'phil': 'python',
-    }
+aliens = []
 
-for language in set(favorite_languages.values()):
-    print(language.title())
+#Make 30 aliens
+
+for val in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+#Make first yellow
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] == 'yellow'
+        alien['speed'] == 'medium'
+        alien['points'] == 10
+
+#Show first 5 
+for val in aliens[:5]:
+    print(val)
+
+print("...")
+
+#Show total number of aliens created
+print(f"Total number of aliens: {len(aliens)}")
+
