ecf5d41b8755f3aa0cacc3d81c445783e7b2354b
thecolorpuce <rileyasp@gmail.com>
2022-07-16 17:26:22 -0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   6/6scrap.py

--- 6/6scrap.py
+++ 6/6scrap.py
@@ -1,20 +1,7 @@
-alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+alien_0 = {'color': 'green', 'points': 5 }
 
-print(f"Original position: {alien_0['x_position']}")
+print(alien_0)
 
-#move to right
-#determine how far to go  with SPEED
+del alien_0['points']
 
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-else:
-    #This boi is quick!
-    x_increment = 3
-
-#The new position is the old position plus the increment
-
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-
-print(f"New position: {alien_0['x_position']}")
+print(alien_0)
