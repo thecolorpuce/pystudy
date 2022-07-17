666ce6a8140a3b36cb75a00b1c35b60c6bbc2300
thecolorpuce <rileyasp@gmail.com>
2022-07-16 20:39:42 -0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   6/6scrap.py 	modified:   Bookmark.txt

--- 6/6scrap.py
+++ 6/6scrap.py
@@ -1,7 +1,5 @@
-alien_0 = {'color': 'green', 'points': 5 }
+alien_0 = {'color': 'green', 'speed':'slow'}    
+print(alien_0['color'])
 
-print(alien_0)
-
-del alien_0['points']
-
-print(alien_0)
+pointValue = alien_0.get('points', 'No point value assigned.')
+print(pointValue)
