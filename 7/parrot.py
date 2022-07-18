cb182d9b215dc5f2a18da02b85ae8babf34843fc
thecolorpuce <rileyasp@gmail.com>
2022-07-17 17:22:38 -0700

    modified:   7/parrot.py

--- 7/parrot.py
+++ 7/parrot.py
@@ -1,10 +1,14 @@
 prompt = "\nTell me something and I will say it back to you: "
 prompt += "\nEnter 'quit' to end the program. "
 
+active = True
+
 message = ""
 
-while message != 'quit':
+while active:
     message = input(prompt)
     
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
