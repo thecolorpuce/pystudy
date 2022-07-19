a5d2b1b3c158ed18e1d2abe5f1dd7a6ff24a7638
thecolorpuce <rileyasp@gmail.com>
2022-07-19 07:55:21 -0700

    modified:   7/7scrap.py 	modified:   7/counting.py 	modified:   Bookmark.txt

--- 7/7scrap.py
+++ 7/7scrap.py
@@ -1,9 +1,5 @@
-prompt = "\nPlease enter the name of a city you have visited:"
-prompt += "\n(Enter 'quit' when you are finished.) "
+x = 1
 
-while True:
-    city = input(prompt)
-    if city == 'quit':
-        break
-    else:
-        print(f"I'd love to go to {city.title()}!")
+while x <= 5:
+    print(x)
+    x = 1
--- 7/counting.py
+++ 7/counting.py
@@ -1,5 +1,8 @@
-currentNumber = 1
+currentNumber = 0
 
-while currentNumber <= 5:
-    print(f"{currentNumber}")
+while currentNumber < 10:
     currentNumber += 1
+    if currentNumber % 2 == 0:
+        continue
+    
+    print(currentNumber)
