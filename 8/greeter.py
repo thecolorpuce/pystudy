ef1ca45af328ae23484e48f51b418e36e57b13e0
thecolorpuce <rileyasp@gmail.com>
2022-07-25 11:12:26 -0700

    new file:   8/greeter.py 	new file:   8/scrap.py 	modified:   Bookmark.txt

---
+++ 8/greeter.py
@@ -0,0 +1,10 @@
+#A simple function that prints a greeting
+
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+    
+greet_user()
+
+for val in range(10):
+    greet_user()
