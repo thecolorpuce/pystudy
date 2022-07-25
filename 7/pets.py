7cdcbb8b73b9ecf3f66a6fb9114e8c9016fd5a69
thecolorpuce <rileyasp@gmail.com>
2022-07-25 08:16:27 -0700

    new file:   7/pets.py

---
+++ 7/pets.py
@@ -0,0 +1,9 @@
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+    
+
+for val in pets:
+    print(f"\n{val.title()}")
