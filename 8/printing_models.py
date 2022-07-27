8f05883cc9fae7cd8b2362c861d4a96803651f06
thecolorpuce <rileyasp@gmail.com>
2022-07-27 10:33:44 -0700

    new file:   8/greetUsers.py 	new file:   8/printing_models.py

---
+++ 8/printing_models.py
@@ -0,0 +1,21 @@
+#Start with some designs that need to be printed
+from locale import currency
+
+
+unprintedDesigns = ['phone case', 'robot pendant', 'dodechedron']
+completeModels = []
+
+#simulate printing each design until none are left
+#move each design to complete_models after printing
+
+while unprintedDesigns:
+    currentDesign = unprintedDesigns.pop()
+    print (f"Printing model: {currentDesign}")
+    completeModels.append(currentDesign)
+    
+#Display Completed Models
+
+print("\nThe following models have been printed: ")
+
+for completedModel in completeModels:
+    print(completeModels)
