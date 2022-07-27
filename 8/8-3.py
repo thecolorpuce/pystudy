6fb3cb0f6bde5da1bc1c9519d9169d30feaa951d
thecolorpuce <rileyasp@gmail.com>
2022-07-27 10:46:44 -0700

    modified:   8/8-3.py

--- 8/8-3.py
+++ 8/8-3.py
@@ -1,8 +1,8 @@
-#8-3. T-Shirt: Write a function called make_shirt() that accepts a size and the
+"""8-3. T-Shirt: Write a function called make_shirt() that accepts a size and the
 #text of a message that should be printed on the shirt. The function should print
 #a sentence summarizing the size of the shirt and the message printed on it.
 #Call the function once using positional arguments to make a shirt. Call the
-#function a second time using keyword arguments.
+#function a second time using keyword arguments."""
 
 def make_shirt(message, size):
     print(f"The shirt will say {message} and be a size {size.title()}")
