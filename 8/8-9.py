f43ccc9c26537a367b70d7484f525e5cc194a960
thecolorpuce <rileyasp@gmail.com>
2022-08-09 10:30:45 -0700

    new file:   8/8-10.py 	new file:   8/8-9.py

---
+++ 8/8-9.py
@@ -0,0 +1,16 @@
+"""
+8-9. Messages: Make a list containing a series of short text messages.
+Pass the list to a function called show_messages(), which prints each text message.
+"""
+
+#This is appropriate for the sake of simplicity
+def messages_messages(messages):
+    """We're just trying to print some messages here. 
+    Unfortunately, I corrode with haste"""
+    
+    for message in messages:
+        print(message)
+
+messages = ['one', 'two', 'three']
+
+messages_messages(messages)
