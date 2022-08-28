db2874cd13de1c4d83e9892a3abb6def4bef0937
thecolorpuce <rileyasp@gmail.com>
2022-08-28 16:03:44 -0500

    Quick Fix to a small problem

--- 6.1/aliens1.py
+++ 6.1/aliens1.py
@@ -2,14 +2,28 @@ aliens = []
 
 #Make 30 green aliens
 
-for v in range(30):
+for alien in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
+#Change the first 3 aliens to the Yellow Setting
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+        print('Test Output') 
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+        print('TEST2')
+
 #Show the first 5 aliens
 
-for v in aliens[:5]:
-    print(aliens)
+for alien in aliens[:5]:
+    print(alien)
     print("\n")
 
 print('....')
