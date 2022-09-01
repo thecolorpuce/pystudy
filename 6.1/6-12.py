68973b120c078daf3200a54d555460ce6c8b468e
thecolorpuce <rileyasp@gmail.com>
2022-09-01 16:01:24 -0500

    Got done with chapter 6 again

---
+++ 6.1/6-12.py
@@ -0,0 +1,7 @@
+""" 
+6-12. Extensions: We’re now working with examples that are complex enough
+that they can be extended in any number of ways. Use one of the example programs 
+from this chapter, and extend it by adding new keys and values, changing the context of the program or improving the formatting of the output. 
+"""
+
+#Nah
