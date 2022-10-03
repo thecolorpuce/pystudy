2a867d5389d4e32bbe49649f17c16da23eaf494f
thecolorpuce <rileyasp@gmail.com>
2022-10-03 17:17:24 -0500

    A stupid script for checking out sweet metrics

---
+++ infinite.py
@@ -0,0 +1,9 @@
+# This only exists so I can check out some radical system monitoring graphs
+
+list = []
+gogo = True
+
+while gogo is True:
+    list.append(1)
+    for i in list:
+        list.append(list)
