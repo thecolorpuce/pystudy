455431f5342785f67ffb896d4f5884b39f16dc06
thecolorpuce <rileyasp@gmail.com>
2022-10-07 12:52:25 -0500

    Adding current progress

---
+++ 9/car2.py
@@ -0,0 +1,45 @@
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model  
+        self.year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        if miles < 0:
+            print("Please provide a value greater than 0.")
+        else:
+            self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class"""
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', '2019')
+print(my_tesla.get_descriptive_name())
---
+++ 9/car4.py
@@ -0,0 +1,72 @@
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model  
+        self.year = year
+        self.odometer_reading = 0
+        self.gas_tank = 0
+    
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        long_name+= f"\nThe gas tank is {self.gas_tank}% full."
+        return long_name
+
+    def fill_gas_tank(self):
+        """Fill the gas tank"""
+        self.gas_tank = 100
+        
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        if miles < 0:
+            print("Please provide a value greater than 0.")
+        else:
+            self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.
+        Then initialize all attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 75
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def fill_gas_tank(Car):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need  a gas tank!")
+
+my_tesla = ElectricCar('tesla', 'model s', '2019')
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+print(my_tesla.get_descriptive_name())
+my_tesla.fill_gas_tank()
+
+print("\n\n")
+
+my_car = Car('Ford', 'Focus', '2013')
+print(my_car.get_descriptive_name())
+my_car.fill_gas_tank()
+print(my_car.get_descriptive_name())
