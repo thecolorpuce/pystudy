99642515b0ffc341ab1ca947b25bdadd6634286b
thecolorpuce <rileyasp@gmail.com>
2022-10-29 14:30:15 -0500

    Changes to alien_invasion, and ship class to facilitate l/r movment

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -20,12 +20,13 @@ class AlienInvasion:
         self.ship = Ship(self)
         self.character = Character(self)
         
+        
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
-            
             
             # Make the most recent drawn screen visible.
             pygame.display.flip()
@@ -35,6 +36,19 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+                
+            # Keydown L/R movement
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            # Key up stop L/R movement
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
