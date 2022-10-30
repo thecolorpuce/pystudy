64533b5b1c87ed57c7386dd9d85622e96186ecb1
thecolorpuce <rileyasp@gmail.com>
2022-10-29 19:57:53 -0500

    Adding new files, including dir 12-6 for the 12-6 challenge

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from character import Character
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets"""
@@ -17,6 +18,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         self.character = Character(self)
         
         
@@ -25,6 +27,14 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+            self._update_screen()
+            
+            # Get rid of bullets that have disappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                print(len(self.bullets))
             self._update_screen()
             
             # Make the most recent drawn screen visible.
@@ -52,6 +62,8 @@ class AlienInvasion:
             self.ship.moving_up = True
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
     
@@ -66,11 +78,24 @@ class AlienInvasion:
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = False
     
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+    
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         self.character.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
